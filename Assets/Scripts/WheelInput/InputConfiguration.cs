03271829a19235d8b130071db7d1c3b2628d65ed
Bot12313 <bot12313@yandex.ru>
2022-01-15 23:29:36 +0700

    Input tests

--- Assets/Scripts/WheelInput/InputConfiguration.cs
+++ Assets/Scripts/WheelInput/InputConfiguration.cs
@@ -1,51 +1,22 @@
 using System;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 namespace WheelInput
 {
     public class InputConfiguration : MonoBehaviour
     {
-        [Serializable]
-        class ActionSetNamePair
-        {
-            public string actionSet;
-            public string actionName;
-
-            public ActionSetNamePair(string actionSet, string actionName)
-            {
-                this.actionSet = actionSet;
-                this.actionName = actionName;
-            }
-
-            public string GetFull(string defaultActionSet) {
-                var aSet = actionSet != "" ? actionSet : defaultActionSet;
-                var fullPath = $"/actions/{aSet}/in/{actionName}";
-                Debug.Log(fullPath);
-                return fullPath;
-            }
-        }
-
         [SerializeField] public SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
         [SerializeField] public SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
         
-        [SerializeField] private string defaultActionSet = "wheelchair";
-        [SerializeField] private ActionSetNamePair firstButtonAction = new ActionSetNamePair(null, "FirstButton");
-
-        [SerializeField] private ActionSetNamePair firstButtonAnalogAction = new ActionSetNamePair(null, "FirstButtonAnalog");
-
-        [SerializeField] private ActionSetNamePair secondButtonAction = new ActionSetNamePair(null, "SecondButton");
-        [SerializeField] private ActionSetNamePair joyStickAction = new ActionSetNamePair(null, "JoyStick");
-        [SerializeField] private ActionSetNamePair joystickTouchedAction = new ActionSetNamePair(null, "JoystickTouched");
-
-        [HideInInspector] public SteamVR_Action_Boolean firstButton;
-        [HideInInspector] public SteamVR_Action_Single firstButtonAnalog;
-        [HideInInspector] public SteamVR_Action_Boolean secondButton;
-        [HideInInspector] public SteamVR_Action_Boolean joystickTouched;
-        [HideInInspector] public SteamVR_Action_Vector2 joyStick;
+        public SteamVR_Action_Boolean firstButton = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("wheelchair","FirstButton");
+        public SteamVR_Action_Single firstButtonAnalog = SteamVR_Input.GetAction<SteamVR_Action_Single>("wheelchair","FirstButtonAnalog");
+        public SteamVR_Action_Boolean secondButton = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("wheelchair","SecondButton");
+        public SteamVR_Action_Boolean joystickTouched = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("wheelchair","JoystickTouched");
+        public SteamVR_Action_Vector2 joyStick = SteamVR_Input.GetAction<SteamVR_Action_Vector2>("wheelchair","JoyStick");
 
         /* -- Singleton */
-
 
         private static InputConfiguration _instance;
 
@@ -69,25 +40,10 @@ namespace WheelInput
         private void Awake()
         {
             Instance = this;
-
-            /* -- */
-
-            InitAllInputs();
-        }
-
-        /* -- */
-
-        private void InitAllInputs()
-        {
-            firstButton = SteamVR_Input.GetAction<SteamVR_Action_Boolean>(firstButtonAction.GetFull(defaultActionSet));
-            // firstButtonAnalog =
-            //     SteamVR_Input.GetAction<SteamVR_Action_Single>(firstButtonAnalogAction.GetFull(defaultActionSet));
-            firstButtonAnalog =
-                SteamVR_Input.GetAction<SteamVR_Action_Single>(firstButtonAnalogAction.actionSet, firstButtonAnalogAction.actionName);
-            secondButton =
-                SteamVR_Input.GetAction<SteamVR_Action_Boolean>(secondButtonAction.GetFull(defaultActionSet));
-            joystickTouched = SteamVR_Input.GetAction<SteamVR_Action_Boolean>(joyStickAction.GetFull(defaultActionSet));
-            joyStick = SteamVR_Input.GetAction<SteamVR_Action_Vector2>(joystickTouchedAction.GetFull(defaultActionSet));
+            SteamVR_Input.GetActionSets().ForEach(actionSet => {
+                Debug.Log(actionSet.fullPath);
+                actionSet.Activate(SteamVR_Input_Sources.Any, 0, false);
+            });
         }
     }
 }
